fdf7678f3488280c4533943c61aec155b43aa692
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-08-29 16:55:29 +0200

    Remove comments

--- Assets/Scripts/Rubiks Cube/Solver/WhiteCross.cs
+++ Assets/Scripts/Rubiks Cube/Solver/WhiteCross.cs
@@ -4,27 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-// Find a middle white plate of row (only row 1 and 3) OR middle white plate of column
-
-// IF ROW
-// a. if is on row 1
-// --> F'
-// --> turn white face until column touch the column that doesn't have white plate
-// --> L'
-// b. else if on row 3
-// --> turn row until column first cube is not white
-// --> F
-// --> turn white face until column touch the column that doesn't have white plate
-// --> R
-
-// IF COLUMN
-// --> turn white face until column touch the column that doesn't have white plate
-// a. if is on left
-// --> L'
-// b. if is on right
-// --> R
-// Place white plate under his desired position
-
 public static class WhiteCross
 {
     static RubiksCube rubiksCube;
