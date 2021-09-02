a65826145cece46e2f5f6602dea0503c0c7c69a3
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-09-03 00:12:49 +0200

    UI changes

--- Assets/Scripts/CameraRotator.cs
+++ Assets/Scripts/CameraRotator.cs
@@ -19,14 +19,14 @@ public class CameraRotator : MonoBehaviour
     private Vector2 swipeDirection; //swipe delta vector2
     private Quaternion cameraRot; // store the quaternion after the slerp operation
     private Touch touch;
-    private float distanceBetweenCameraAndTarget;
+    private float distanceBetweenCameraAndTarget = 25f;
 
     private float minXRotAngle = -80; //min angle around x axis
     private float maxXRotAngle = 80; // max angle around x axis
 
     //Mouse rotation related
-    private float rotX = 16; // around x
-    private float rotY = 33; // around y
+    private float rotX = 7.207f; // around x
+    private float rotY = 21.71f; // around y
     private void Awake()
     {
         if (mainCamera == null)
